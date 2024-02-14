bee6ead36301ba7fdb8264e61e335657b314840e
dbermudez <dbermudez5804@westfield.ma.edu>
2024-02-14 10:31:04 -0500

    Added more techniques + descriptions

--- src/data/techniques.ts
+++ src/data/techniques.ts
@@ -1,52 +1,42 @@
 export interface Technique {
   name: string;
+  shortDescription: string;
+  description: string;
   id: number;
 }
 
 const techniques: Technique[] = [
   {
     name: 'Pomodoro',
+    shortDescription: "25 minute stretches with 5 minute breaks.",
+    description: "Pick a task to work on for 25 minutes with 5 minute breaks between rounds.",
     id: 0
   },
   {
     name: 'Flowtime',
+    shortDescription: "Blend task breakdown with flexibility of work intervals.",
+    description: "Unlike Pomodoro, you set your own work time and break time sessions.",
     id: 1
   },
   {
     name: 'Active Recall',
+    shortDescription: "Free timer to allow for reiteration of your knowledge to yourself.",
+    description: "Pick a topic that you wish to learn, create questions, then quiz yourself on those questions with a timer you can control!",
     id: 2
 
   },
-//   {
-//     fromName: 'Bill Thomas',
-//     subject: 'The situation',
-//     date: 'Yesterday',
-//     id: 3
-//   },
-//   {
-//     fromName: 'Joanne Pollan',
-//     subject: 'Updated invitation: Swim lessons',
-//     date: 'Yesterday',
-//     id: 4
-//   },
-//   {
-//     fromName: 'Andrea Cornerston',
-//     subject: 'Last minute ask',
-//     date: 'Yesterday',
-//     id: 5
-//   },
-//   {
-//     fromName: 'Moe Chamont',
-//     subject: 'Family Calendar - Version 1',
-//     date: 'Last Week',
-//     id: 6
-//   },
-//   {
-//     fromName: 'Kelly Richardson',
-//     subject: 'Placeholder Headhots',
-//     date: 'Last Week',
-//     id: 7
-//   }
+  {
+    name: 'Feynman Technique',
+    shortDescription: "Free timer to allow teaching someone else.",
+    description: "Pick a topic and use what you learned to teach someone else with a timer that you can control!",
+    id: 3
+  },
+  {
+    name: "52-17",
+    shortDescription: "Longer than Pomodoro! 52 minute stretches with 17 minute breaks.",
+    description: "If you want to challenge yourself, pick a task to work on for 52 minutes with 17 minute breaks between rounds!",
+    id: 4
+  },
 ];
 
 export const getTechniques = () => techniques;
