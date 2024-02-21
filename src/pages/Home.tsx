3aaf019986e8502d567f0f39f1b27392343c2a33
Logan St. Clair <loganst.clair@Macbook69.local>
2024-02-21 11:16:22 -0500

    Added Card as well as settings Button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,15 +4,27 @@ import { Technique, getTechniques } from '../data/techniques';
 import {
   IonContent,
   IonHeader,
+  IonCard,
+  IonIcon,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonCardContent,
+  IonButton,
+  IonRouterLink,
   IonList,
   IonPage,
   IonRefresher,
+  IonLabel,
+  IonThumbnail,
+  IonItem,
   IonRefresherContent,
   IonTitle,
   IonToolbar,
   useIonViewWillEnter
 } from '@ionic/react';
 import './Home.css';
+import { settings, star } from 'ionicons/icons';
 
 const Home: React.FC = () => {
 
@@ -33,7 +45,10 @@ const Home: React.FC = () => {
     <IonPage id="home-page">
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Home</IonTitle>
+          <IonTitle>Study Home</IonTitle>
+            <IonButton>
+              <IonIcon mode="ios" slot="icon-only" icon={settings}></IonIcon>
+            </IonButton>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
@@ -52,6 +67,33 @@ const Home: React.FC = () => {
         <IonList>
             {techniques.map(t => <StudyTechniques key={t.id} technique={t} />)}
         </IonList>
+        
+        <IonCard mode="ios">
+          <IonCardHeader>
+            <IonCardTitle>Promordo</IonCardTitle>
+            <IonCardSubtitle>25 min, 5 min</IonCardSubtitle>
+          </IonCardHeader>
+
+          <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
+          <IonRouterLink routerDirection="forward"> </IonRouterLink>
+          <IonButton routerLink={`/technique/${0}`} expand="block" fill="solid">START STUDYING</IonButton>
+
+        </IonCard>
+
+        <IonCard mode="ios">
+          <IonCardHeader>
+            <IonCardTitle>FlowTime</IonCardTitle>
+            <IonCardSubtitle>Working Intervals</IonCardSubtitle>
+          </IonCardHeader>
+
+          <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
+          <IonRouterLink routerDirection="forward"> </IonRouterLink>
+          <IonButton routerLink={`/technique/${1}`} expand="block" fill="solid">START STUDYING</IonButton>
+
+        </IonCard>
+
+
+
       </IonContent>
     </IonPage>
   );
